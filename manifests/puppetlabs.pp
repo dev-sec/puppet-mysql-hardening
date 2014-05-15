3fd0a5d3430aef2ae3686dc96154f270f9f11803
Dominik Richter <dominik.richter@gmail.com>
2014-05-15 23:28:34 +0200

    structure core steps for overriding mysql options
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- manifests/puppetlabs.pp
+++ manifests/puppetlabs.pp
@@ -12,5 +12,17 @@
 #
 class mysql_hardening::puppetlabs(
 ) {
-  # ...
+  # hardening options
+  $hardening_oo = {
+    # ...
+  }
+
+  # override_options
+  $org_oo = getparam(Class['::mysql::server'], "override_options")
+  $nu_oo = merge_hardening( $org_oo, $hardening_oo )
+
+  # Resource-usage mode
+  Class['::mysql::server'] {
+    override_options => $nu_oo
+  }
 }
